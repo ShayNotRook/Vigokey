5a91b7a40bc4f64e8faf75a1521ca5938d74ae47
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-01-16 18:43:59 +0330

    Added users viewset and a url for admins to see registered users

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+
+from .models import CustomUser as User
+from .serializers import UserSerializer
 # Create your views here.
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
