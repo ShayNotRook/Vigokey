f37dc862a5b528c045d1af67f435c152f7f3979f
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-02-11 14:28:05 +0330

    Minor changes to models.

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
@@ -6,7 +7,24 @@ def user_image_path(instance, filename):
     return f'user_{instance.id}/{filename}'
 
 class CustomUser(AbstractUser):
+    name = models.CharField(("Name of user"), blank=True, null=True, max_length=255)
+    is_verified = models.BooleanField(default=False)
+    
+    # def get_absolute_url(self):
+    #     return reverse('accounts:detail', kwargs={'username': self.username})
+    
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
     profile_pic = models.ImageField(upload_to=user_image_path, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    newsletter_status = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.user.email
+    
+    class Meta:
+        verbose_name_plural = 'User Profile'
     
     
 class Wallet(models.Model):
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
 from .views import UserViewSet
 
+
+app_name = 'accounts'
+
+
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='users')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
